a79e770fd54bf3d3a1b408e49bf4d470b32ef6cb
LizKamugisha <liz.kamugisha@gmail.com>
2021-07-13 14:17:53 +0300

    Combined Models

--- model/addmarketmodel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const addmarketsSchema = new mongoose.Schema({
-  market_name: String,
-  location: String,
-  uploadimage: String,
-});
-module.exports = mongoose.model('Addmarket', addmarketsSchema);
--- model/addproductmodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const addproductsSchema = new mongoose.Schema({
-  productname: String,
-  producttype: String,
-  productprice: String,
-  uploadimage: String,
-});
-module.exports = mongoose.model('Addproduct', addproductsSchema);
